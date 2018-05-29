e51d64d52e5b4f499eb77ce5f5b01c9d301035fa
Piotr Nowojski <piotr.nowojski@gmail.com>
2018-05-29 08:40:50 +0200

    mid work epoll

---
+++ flink-tests/src/test/java/org/apache/flink/test/runtime/NettyEpollITCase.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.test.runtime;
+
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.test.util.MiniClusterResource;
+import org.apache.flink.util.TestLogger;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.flink.runtime.io.network.netty.NettyConfig.TRANSPORT_TYPE;
+
+/**
+ * Manually test the throughput of the network stack.
+ */
+public class NettyEpollITCase extends TestLogger {
+
+	private static final Logger LOG = LoggerFactory.getLogger(NettyEpollITCase.class);
+
+	private static final int TASK_MANAGERES = 2;
+
+	private static MiniClusterResource cluster;
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		System.setProperty(
+			"org.apache.flink.shaded.netty4.io.netty.packagePrefix",
+			"org.apache.flink.shaded.netty4.");
+		System.setProperty(
+			"io.netty.packagePrefix",
+			"org.apache.flink.shaded.netty4.");
+		cluster = new MiniClusterResource(
+			new MiniClusterResource.MiniClusterResourceConfiguration(
+				getConfiguration(),
+				TASK_MANAGERES,
+				1),
+			true);
+		cluster.before();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		cluster.after();
+	}
+
+	@Test
+	public void testNettyEpoll() throws Exception {
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setParallelism(TASK_MANAGERES);
+		env.getConfig().disableSysoutLogging();
+
+		DataStream<Integer> input = env.fromElements(1, 2, 3, 4, 1, 2, 3, 42);
+		input.keyBy(new KeySelector<Integer, Integer>() {
+			@Override
+			public Integer getKey(Integer value) throws Exception {
+				return value;
+			}
+		})
+			.sum(0)
+			.printToErr();
+
+		env.execute();
+	}
+
+	private static Configuration getConfiguration() {
+		Configuration config = new Configuration();
+		config.setString(TRANSPORT_TYPE, "epoll");
+		return config;
+	}
+}
