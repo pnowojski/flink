a1ff1048dfae551fd2bcff2b9ae9756930cf69f0
Piotr Nowojski <piotr.nowojski@gmail.com>
2019-10-29 14:57:14 +0100

    Initial SourceFunctionReaderOperator

---
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/SourceFunctionReaderOperator.java
@@ -0,0 +1,214 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.api.operators;
+
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.runtime.state.StateInitializationContext;
+import org.apache.flink.runtime.state.StateSnapshotContext;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.graph.StreamConfig;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.runtime.io.InputStatus;
+import org.apache.flink.streaming.runtime.streamrecord.LatencyMarker;
+import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.apache.flink.streaming.runtime.streamstatus.StreamStatusMaintainer;
+import org.apache.flink.streaming.runtime.tasks.StreamTask;
+import org.apache.flink.util.FlinkException;
+import org.apache.flink.util.OutputTag;
+
+import javax.annotation.Nullable;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+
+import static org.apache.flink.util.Preconditions.checkState;
+
+/**
+ * Handover wrapper of {@link SourceFunction} into a {@link SourceReaderOperator}.
+ *
+ * It creates a separate thread for the {@link SourceFunction} and a special handover buffer.
+ * Every time SourceFunction collects an element, it is passed to the the handover buffer.
+ * This handover buffer is then used by {@link SourceReaderOperator} to emitNext elements.
+ */
+@Internal
+public class SourceFunctionReaderOperator<OUT> extends SourceReaderOperator<OUT>  {
+
+	private final Queue<StreamRecord<OUT>> handoverBuffer = new ArrayDeque<>();
+	private final HandoverOutput handoverOutput = new HandoverOutput();
+	private final StreamSource<OUT, ?> streamSource;
+	private final AvailabilityHelper availabilityHelper = new AvailabilityHelper();
+
+	/**
+	 * 	TODO: move to {@link StreamOperatorFactory} and make those fields final.
+	 */
+	private transient LegacySourceFunctionThread sourceFunctionThread;
+	private transient Object checkpointLock;
+
+	private boolean isFinished;
+	@Nullable
+	private Throwable asyncException;
+
+	public SourceFunctionReaderOperator(SourceFunction<OUT> sourceFunction) {
+		this.streamSource = new StreamSource<>(sourceFunction);
+	}
+
+	@Override
+	public InputStatus emitNext(DataOutput<OUT> output) throws Exception {
+		synchronized (checkpointLock) {
+			checkErroneous();
+
+			if (!handoverBuffer.isEmpty()) {
+				output.emitRecord(handoverBuffer.poll());
+			}
+
+			if (!handoverBuffer.isEmpty()) {
+				return InputStatus.MORE_AVAILABLE;
+			}
+			else if (!isFinished) {
+				availabilityHelper.resetUnavailable();
+				return InputStatus.NOTHING_AVAILABLE;
+			}
+			else  {
+				return InputStatus.END_OF_INPUT;
+			}
+		}
+	}
+
+	private void checkErroneous() throws FlinkException {
+		if (asyncException != null) {
+			throw new FlinkException("SourceFunction thread failed.", asyncException);
+		}
+	}
+
+	@Override
+	public void setup(
+			StreamTask<?, ?> containingTask,
+			StreamConfig config,
+			Output<StreamRecord<OUT>> output) {
+		super.setup(containingTask, config, output);
+
+		streamSource.setup(containingTask, config, output);
+		checkpointLock = containingTask.getCheckpointLock();
+		sourceFunctionThread = new LegacySourceFunctionThread(containingTask.getStreamStatusMaintainer());
+
+		sourceFunctionThread.run();
+	}
+
+	@Override
+	public void initializeState(StateInitializationContext context) throws Exception {
+		checkState(Thread.holdsLock(checkpointLock));
+		checkErroneous();
+		streamSource.initializeState(context);
+	}
+
+	@Override
+	public void snapshotState(StateSnapshotContext context) throws Exception {
+		checkState(Thread.holdsLock(checkpointLock));
+		checkErroneous();
+		streamSource.snapshotState(context);
+	}
+
+	@Override
+	public CompletableFuture<?> isAvailable() {
+		return availabilityHelper.isAvailable();
+	}
+
+	@Override
+	public void close() throws Exception {
+		sourceFunctionThread.join();
+		streamSource.close();
+	}
+
+	public void cancel() {
+		streamSource.cancel();
+	}
+
+	private void legacySourceFunctionThreadCompleted() {
+		synchronized (checkpointLock) {
+			isFinished = true;
+		}
+	}
+
+	private void legacySourceFunctionThreadCompleteExceptionally(Throwable t) {
+		synchronized (checkpointLock) {
+			isFinished = true;
+			asyncException = t;
+		}
+	}
+
+	/**
+	 * Runnable that executes the the source function in the head operator.
+	 */
+	private class LegacySourceFunctionThread<OUT> extends Thread {
+
+		private final StreamStatusMaintainer streamStatusMaintainer;
+
+		LegacySourceFunctionThread(StreamStatusMaintainer streamStatusMaintainer) {
+			this.streamStatusMaintainer = streamStatusMaintainer;
+		}
+
+		@Override
+		public void run() {
+			try {
+				streamSource.run(checkpointLock, streamStatusMaintainer, handoverOutput, null);
+				legacySourceFunctionThreadCompleted();
+			} catch (Throwable t) {
+				legacySourceFunctionThreadCompleteExceptionally(t);
+			}
+		}
+
+		public void setTaskDescription(final String taskDescription) {
+			setName("Legacy Source Thread - " + taskDescription);
+		}
+	}
+
+	/**
+	 * Calls to the methods of this class are protected by {@code checkpointLock} acquired in
+	 * {@link org.apache.flink.streaming.api.operators.StreamSourceContexts.WatermarkContext}.
+	 */
+	private class HandoverOutput implements Output<StreamRecord<OUT>> {
+		@Override
+		public void emitWatermark(Watermark mark) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public <X> void collect(OutputTag<X> outputTag, StreamRecord<X> record) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void emitLatencyMarker(LatencyMarker latencyMarker) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void collect(StreamRecord<OUT> record) {
+			boolean wasEmpty = handoverBuffer.isEmpty();
+			handoverBuffer.add(record);
+			if (wasEmpty) {
+				availabilityHelper.getUnavailableToResetAvailable().complete(null);
+			}
+		}
+
+		@Override
+		public void close() {
+		}
+	}
+}
