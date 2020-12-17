9effb9156516736de950bc1eda09742427f6ad84
Piotr Nowojski <piotr.nowojski@gmail.com>
2020-12-17 16:22:43 +0100

    run WebUI in WordCount

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/wordcount/WordCount.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/wordcount/WordCount.java
@@ -20,8 +20,11 @@ package org.apache.flink.streaming.examples.wordcount;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.MultipleParameterTool;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.examples.wordcount.util.WordCountData;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.Preconditions;
@@ -55,8 +58,11 @@ public class WordCount {
         final MultipleParameterTool params = MultipleParameterTool.fromArgs(args);
 
         // set up the execution environment
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Configuration configuration = new Configuration();
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
 
+        env.setParallelism(8);
         // make parameters available in the web interface
         env.getConfig().setGlobalJobParameters(params);
 
@@ -76,7 +82,27 @@ public class WordCount {
             System.out.println("Executing WordCount example with default input data set.");
             System.out.println("Use --input to specify file input.");
             // get default test text data
-            text = env.fromElements(WordCountData.WORDS);
+            text =
+                    env.addSource(
+                            new SourceFunction<String>() {
+                                private volatile boolean running = true;
+
+                                @Override
+                                public void run(SourceContext<String> ctx) throws Exception {
+                                    while (running) {
+                                        for (String word : WordCountData.WORDS) {
+                                            synchronized (ctx.getCheckpointLock()) {
+                                                ctx.collect(word);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void cancel() {
+                                    running = false;
+                                }
+                            });
         }
 
         DataStream<Tuple2<String, Integer>> counts =
@@ -91,7 +117,17 @@ public class WordCount {
             counts.writeAsText(params.get("output"));
         } else {
             System.out.println("Printing result to stdout. Use --output to specify output path.");
-            counts.print();
+            counts.addSink(
+                    new RichSinkFunction<Tuple2<String, Integer>>() {
+                        @Override
+                        public void invoke(Tuple2<String, Integer> value) throws Exception {
+                            if (getRuntimeContext().getIndexOfThisSubtask() == 1) {
+                                Thread.sleep(1);
+                            }
+                            // ignore
+                        }
+                    });
+            //			counts.print();
         }
         // execute program
         env.execute("Streaming WordCount");
